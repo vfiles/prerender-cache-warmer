26d68b1addca3004bb8580f0a6dcc97811c9e275
Evan Drewry <evan@vfiles.com>
2015-08-20 19:21:42 -0400

    clean up Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -14,16 +14,22 @@ Portability : POSIX
 module Main where
 
 ------------------------------------------------------------------------------
-import Network.HTTP.Conduit (parseUrl, http, Request(..), Response(..), Manager, tlsManagerSettings, newManager)
-import Network.HTTP.Types.Status (Status(..))
-import System.Environment   (getArgs)
+import Control.Concurrent.Async     (mapConcurrently)
+import Control.Exception.Extra      (retry)
 import Control.Monad.Trans.Resource (runResourceT)
-import Control.Monad.Trans.Class (lift)
-import Control.Concurrent.Async (mapConcurrently)
-import Data.Time (getCurrentTime, diffUTCTime)
-import Data.List.Split (chunksOf)
-import Control.Exception.Extra (retry)
-
+import Control.Monad.Trans.Class    (lift)
+import Data.List.Split              (chunksOf)
+import Data.Time                    (getCurrentTime
+                                    ,diffUTCTime)
+import Network.HTTP.Conduit         (parseUrl
+                                    ,http
+                                    ,Request(..)
+                                    ,Response(..)
+                                    ,Manager
+                                    ,tlsManagerSettings
+                                    ,newManager)
+import Network.HTTP.Types.Status    (Status(..))
+import System.Environment           (getArgs)
 
 
 readLines :: FilePath -> IO [String]
@@ -32,13 +38,15 @@ readLines = fmap lines . readFile
 crawl manager url = do
   start <- getCurrentTime
   initReq <- parseUrl url
-  let req = initReq {
-              requestHeaders = [("User-Agent", "Googlebot/2.1 (+http://www.google.com/bot.html)")]
-            }
+  let hdr = [("User-Agent", "Googlebot/2.1 (+http://www.google.com/bot.html)")]
+      req = initReq { requestHeaders = hdr }
   runResourceT $ do
     resp <- http req manager
     stop <- lift getCurrentTime
-    lift $ putStrLn $ "got status " ++ (show $ statusCode $ responseStatus resp) ++ " from " ++ url ++ " in " ++ (show $ diffUTCTime stop start)
+    let elapsed = diffUTCTime stop start
+        status  = statusCode $ responseStatus resp
+        msg = (show status) ++ " from " ++ url ++ " in " ++ (show elapsed)
+    lift $ putStrLn msg
 
 crawlConcurrently manager urls = do
   mapM_ (mapConcurrently $ retry 3 . crawl manager) (chunksOf 100 urls)
